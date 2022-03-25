7edfd7f3a74368155d15238f17e74ecede5171f7
Roman Korneev <rm@ipadla.org>
2022-03-25 12:29:23 +0300

    Completed homework. Attempt 3

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -7,7 +7,7 @@ class IsObjectAuthor(permissions.BasePermission):
     def has_permission(self, request, view):
         return (
             request.method in permissions.SAFE_METHODS
-            or bool(request.user and request.user.is_authenticated)
+            or request.user.is_authenticated
         )
 
     def has_object_permission(self, request, view, obj):
